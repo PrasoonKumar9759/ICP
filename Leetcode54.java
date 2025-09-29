e7f68b9868b795c09a3c85c94137c284f045507f
PrasoonKumar9759 <pirsoonkumar@gmail.com>
2025-09-29 23:26:02 +0530

    adq

---
+++ Leetcode54.java
@@ -0,0 +1,36 @@
+class Solution {
+    public static void main(String[] args){
+        int[][] matrix={{1,2,3,4},{5,6,7,8},{9,10,11,12}}
+    }
+    public List<Integer> spiralOrder(int[][] arr) {
+        List<Integer> list=new ArrayList<>();
+       int m=arr.length, n=arr[0].length;
+       int minr=0,maxr=arr.length-1;
+       int minc=0,maxc=arr[0].length-1;
+       int total=m*n,c=0;
+       while(c<total && minr<=maxr && minc<=maxc){
+        for(int i=minc;i<=maxc && c<total;i++){
+            list.add(arr[minr][i]);
+            c++;
+        }
+        minr++;
+        for(int i=minr;i<=maxr && c<total;i++){
+            list.add(arr[i][maxc]);
+            c++;
+        }
+        maxc--;
+        for(int i=maxc;i>=minc && c<total;i--){
+            list.add(arr[maxr][i]);
+            c++;
+        }
+        maxr--;
+        for(int i=maxr;i>=minr && c<total;i--){
+            list.add(arr[i][minc]);
+            c++;
+        }
+        minc++;
+       }
+       return list;
+    }
+
+}
---
+++ Leetcode74.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static void main(String[] args){
+        int[][] matrix={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        System.out.println(searchMatrix(matrix,10));
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int col=matrix[0].length-1, row=0;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]<target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+    return false;
+    }
+}
